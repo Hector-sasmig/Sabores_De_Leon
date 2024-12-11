de16ddc8073093830b248b3550f835b992b67ea0
Héctor Sastre Miguélez <91277857+Hector-sasmig@users.noreply.github.com>
2024-12-11 18:38:52 +0100

    DAOs

--- INSO_HOR_SaboresDeLeon/src/modelo/dao/EmpleadoDAO.java
+++ INSO_HOR_SaboresDeLeon/src/modelo/dao/EmpleadoDAO.java
@@ -1,5 +1,89 @@
 package modelo.dao;
 
-public class EmpleadoDAO {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import modelo.conexion.Conexion;
+import modelo.vo.EmpleadoVO;
+
+public class EmpleadoDAO extends Conexion{
+
+	public ArrayList<EmpleadoVO> getLista() throws Exception {
+		try {
+			this.abrirConexion();
+			PreparedStatement st = this.getConnection().prepareStatement("SELECT * FROM EMPLEADO");
+			ResultSet rs = st.executeQuery();
+			ArrayList<EmpleadoVO> empleados = new ArrayList<EmpleadoVO>();
+			while (rs.next()) {
+				EmpleadoVO empleado = new EmpleadoVO(rs.getString("Nombre"), rs.getString("Apellido"), rs.getString("Dni"), rs.getInt("Telefono"), rs.getString("Correo"), rs.getString("Tipo"), rs.getString("Contrasena"));
+				empleado.setIdEmpleado(rs.getInt("id"));;
+				empleados.add(empleado);
+			}
+			this.cerrarConexion();
+			return empleados;
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+	}
+	
+	public EmpleadoVO getEmpleado(int id) throws Exception {
+ 		try {
+ 			this.abrirConexion();
+ 			PreparedStatement st = this.getConnection().prepareStatement("SELECT * FROM EMPLEADO WHERE id=? ");
+ 			st.setInt(1, id);
+ 			ResultSet rs = st.executeQuery();
+ 			EmpleadoVO empleado = null;
+ 			
+ 			while (rs.next()) {
+ 				empleado= new EmpleadoVO(rs.getString("Nombre"), rs.getString("Apellido"), rs.getString("Dni"), rs.getInt("Telefono"), rs.getString("Correo"), rs.getString("Tipo"), rs.getString("Contrasena"));
+ 			}
+ 			
+ 			this.cerrarConexion();
+ 			return empleado;
+ 		} catch (Exception e) {
+ 			throw new Exception(e);
+ 		}
+ 	}
+	
+	public void add(EmpleadoVO empleado) throws Exception {
+		try {
+			this.abrirConexion();
+			PreparedStatement st = this.getConnection().prepareStatement("INSERT INTO EMPLEADO"+"(nombre,apellido,dni,telefono,correo,tipo,contrasena) VALUES(?,?,?,?,?,?,?)");
+			st.setString(1, empleado.getNombre());
+			st.setString(2, empleado.getApellido());
+			st.setString(3, empleado.getDni());
+			st.setInt(4, empleado.getTelefono());
+			st.setString(5, empleado.getCorreo());
+			st.setString(6, empleado.getTipo());
+			st.setString(7, empleado.getContrasena());
+			if(st.executeUpdate()==0) {
+				//Error
+			} else {
+				this.cerrarConexion();
+			}
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+	}
+	
+	public void update(EmpleadoVO empleadoVO) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			this.abrirConexion();
+			PreparedStatement st = this.getConnection().prepareStatement("UPDATE EMPLEADO SET nombre = ?, apellido = ?, dni = ?, telefono = ?, correo = ?, tipo = ?, contrasena = ? WHERE id = ?");
+			st.setString(1, empleadoVO.getNombre());
+			st.setString(2, empleadoVO.getApellido());
+			st.setString(3, empleadoVO.getDni());
+			st.setInt(4, empleadoVO.getTelefono());
+			st.setString(5, empleadoVO.getCorreo());
+			st.setString(6, empleadoVO.getTipo());
+			st.setString(7, empleadoVO.getContrasena());
+			st.setInt(8, empleadoVO.getIdEmpleado());
+			st.executeUpdate();
+			
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+	}
 }
--- INSO_HOR_SaboresDeLeon/src/modelo/dao/MenuDAO.java
+++ INSO_HOR_SaboresDeLeon/src/modelo/dao/MenuDAO.java
@@ -1,5 +1,84 @@
 package modelo.dao;
 
-public class MenuDAO {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import modelo.conexion.Conexion;
+import modelo.vo.MenuVO;
+
+public class MenuDAO extends Conexion {
+
+	public ArrayList<MenuVO> getLista() throws Exception {
+		try {
+			this.abrirConexion();
+			PreparedStatement st = this.getConnection().prepareStatement("SELECT * FROM MENU");
+			ResultSet rs = st.executeQuery();
+			ArrayList<MenuVO> menus = new ArrayList<MenuVO>();
+			while (rs.next()) {
+				MenuVO menu = new MenuVO(rs.getString("PrimerPlato"), rs.getString("SegundoPlato"), rs.getString("Postre"), rs.getString("Bebida"), rs.getFloat("Precio"));
+				menu.setIdMenu(rs.getInt("id"));
+				menus.add(menu);
+			}
+			this.cerrarConexion();
+			return menus;
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+	}
+	
+	public MenuVO getMenu(int id) throws Exception {
+		try {
+			this.abrirConexion();
+			PreparedStatement st = this.getConnection().prepareStatement("SELECT * FROM MENU WHERE id = ?");
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+			MenuVO menu = null;
+			while (rs.next()) {
+				menu = new MenuVO(rs.getString("PrimerPlato"), rs.getString("SegundoPlato"), rs.getString("Postre"), rs.getString("Bebida"), rs.getFloat("Precio"));
+				menu.setIdMenu(rs.getInt("id"));
+			}
+			this.cerrarConexion();
+			return menu;
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+	}
+
+	public void add(MenuVO menu) throws Exception {
+		try {
+			this.abrirConexion();
+			PreparedStatement st = this.getConnection().prepareStatement(
+					"INSERT INTO MENU" + "(PrimerPlato,SegundoPlato,Postre,Bebida,Precio) VALUES(?,?,?,?,?)");
+			st.setString(1, menu.getPrimerPlato());
+			st.setString(2, menu.getSegundoPlato());
+			st.setString(3, menu.getPostre());
+			st.setString(4, menu.getBebida());
+			st.setFloat(5, menu.getPrecio());
+			if (st.executeUpdate() == 0) {
+				// Error
+			} else {
+				this.cerrarConexion();
+			}
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+	}
+
+	public void update(MenuVO menu) throws Exception {
+		try {
+			this.abrirConexion();
+			PreparedStatement st = this.getConnection().prepareStatement(
+					"UPDATE MENU SET primerPlato=?, segundoPlato=?, postre=?, bebida=?, precio=? WHERE id=?");
+			st.setString(1, menu.getPrimerPlato());
+			st.setString(2, menu.getSegundoPlato());
+			st.setString(3, menu.getPostre());
+			st.setString(4, menu.getBebida());
+			st.setFloat(5, menu.getPrecio());
+			st.setInt(6, menu.getIdMenu());
+			st.executeUpdate();
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+	}
 }
