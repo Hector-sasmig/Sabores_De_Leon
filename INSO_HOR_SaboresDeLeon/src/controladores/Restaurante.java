97f66e46cf2464ebdbc9f960569129229a01a7a3
Héctor Sastre Miguélez <91277857+Hector-sasmig@users.noreply.github.com>
2024-12-30 12:43:16 +0100

    Controlador Logging

--- INSO_HOR_SaboresDeLeon/src/controladores/Restaurante.java
+++ INSO_HOR_SaboresDeLeon/src/controladores/Restaurante.java
@@ -1,10 +1,12 @@
 package controladores;
 
+import vista.LoggingVista;
 
 public class Restaurante {
 
 	public static void main(String[] args) {
-		
+		LoggingVista lv = new LoggingVista();
+		lv.setVisible(true);
 		
 	}
 }
